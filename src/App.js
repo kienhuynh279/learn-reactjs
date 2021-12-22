43d64ec8b26544b9f7698511a3b411877d1e946d
Kien Huynh <kienhuynh279@gmail.com>
2021-12-22 22:09:27 +0700

    Learn useEffect: lab PostList and Pagionation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 // import './App.css';
-import TodoFeature from "./features/Todo";
+import { useEffect, useState } from "react";
+import queryString from 'query-string'
+import PostList from "./features/PostList";
+import Pagination from './features/Pagination'
 
 function App() {
+  const [postList, setPostList] = useState([])
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 49
+  })
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1
+  })
+
+  useEffect(() => {
+    async function fetchPostList () {
+      try {
+        const paramString = queryString.stringify(filters)
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`
+        const response = await fetch(requestUrl)
+        const responseJSON = await response.json()
+
+        const { data, pagination } = responseJSON
+        setPostList(data)
+        setPagination(pagination)
+      }
+      catch(error){
+        console.log('Falied');
+      }
+    }
+
+    fetchPostList()
+  }, [filters])
+
+  function handlePageChange(newPage) {
+    console.log('New  Page: ', newPage);
+    setFilters({
+      ...filters,
+      _page: newPage
+    })
+  }
+
   return (
     <div className="App">
-      <TodoFeature></TodoFeature>
+      <h1>
+        React Hooks
+      </h1>
+      <PostList posts={postList} />
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      ></Pagination>
+      {/* <TodoFeature></TodoFeature> */}
       {/* <ColorBox></ColorBox> */}
     </div>
   );
---
+++ src/features/Pagination/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Pagonation.propTypes = {
+  pagination: PropTypes.object.isRequired,
+  onPageChange: PropTypes.func,
+};
+
+Pagonation.defaultProps = {
+  onPageChange: null,
+};
+
+function Pagonation(props) {
+  const { pagination, onPageChange } = props;
+  const { _page, _limit, _totalRows } = pagination;
+  const totalPage = Math.ceil(_totalRows / _limit);
+
+  function handlePageChange(newPage) {
+    if (onPageChange) {
+      onPageChange(newPage);
+    }
+  }
+
+  return (
+    <div>
+      <button disabled={_page <= 1} onClick={() => handlePageChange(_page - 1)}>
+        Prev
+      </button>
+      <h5>{_page}</h5>
+      <button
+        disabled={_page >= totalPage}
+        onClick={() => handlePageChange(_page + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Pagonation;

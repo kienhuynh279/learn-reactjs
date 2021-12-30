40037f4e098c7df4719c30726fe0816b76d3e6ee
Kien Huynh <kienhuynh279@gmail.com>
2021-12-30 21:40:46 +0700

    Learn ReactJS: Routing in React

--- src/App.js
+++ src/App.js
@@ -3,25 +3,43 @@
 // import ClockFeature from "./features/Clock";
 // import PostListFeature from "./features/PostList";
 
+import { NavLink, Link, Route, Switch } from "react-router-dom";
+import TodoFeature from "./features/Todo";
+import SongFeature from "./features/Song";
+import BetterClockFeature from "./features/BetterClock";
+import PostListFeature from "./features/PostList";
 import MagicBoxFeature from "./features/MagicBox";
+import "./App.css";
 
 function App() {
   // const [showClock, setShowClock] = useState(true)
 
   return (
     <div className="App">
-      <h1>
-        React Hooks
-      </h1>
-
-      
-      {/* <PostListFeature></PostListFeature>
-      {showClock && <ClockFeature></ClockFeature>}
-      
-      <BetterClockFeature></BetterClockFeature>
-      <button onClick={() => {setShowClock(false)}}>Hide Clock</button> */}
-
-      <MagicBoxFeature></MagicBoxFeature>
+      <div>
+        <p>
+          <NavLink to="/todos">Todo</NavLink>
+        </p>
+        <p>
+          <NavLink to="/songs">Songs</NavLink>
+        </p>
+        <p>
+          <NavLink to="/clock">Clock</NavLink>
+        </p>
+        <p>
+          <NavLink to="/posts">Posts</NavLink>
+        </p>
+        <p>
+          <NavLink to="/magic-box">Magic Box</NavLink>
+        </p>
+      </div>
+      <Switch>
+        <Route path="/todos" component={TodoFeature}></Route>
+        <Route path="/songs" component={SongFeature}></Route>
+        <Route path="/clock" component={BetterClockFeature}></Route>
+        <Route path="/posts" component={PostListFeature}></Route>
+        <Route path="/magic-box" component={MagicBoxFeature}></Route>
+      </Switch>
     </div>
   );
 }
